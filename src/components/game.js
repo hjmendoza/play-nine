9cbd08e2e0b8dd3648d1f0bd1a2ea76284d29a8b
Haley Mendoza <mendozahaley@gmail.com>
2019-01-15 10:50:37 -0800

    ifimproved file strucutre. added game component

---
+++ src/components/game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Play Nine</h1>
+      </div>
+    )
+  }
+}
+
+export default Game
