7299e00d9b8d6754bdff6591bc5d8458367fe377
Haley Mendoza <mendozahaley@gmail.com>
2019-02-10 10:14:25 -0800

    selected numbers bug fix - reselections not allowed

--- src/components/game.js
+++ src/components/game.js
@@ -12,6 +12,7 @@ class Game extends Component {
   };
 
   selectedNumber = (clickedNumber) => {
+    if(this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return ;}
     this.setState(prevState => ({
       selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
     }))
