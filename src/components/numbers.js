0834564cb0c0a932c814bad0d0295908a7bde6af
Haley Mendoza <mendozahaley@gmail.com>
2019-02-10 16:57:03 -0800

    bug fix - prevent used number resuse

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -2,27 +2,28 @@ import React from 'react';
 // import { Range } from 'react-lodash';
 
 const Numbers = (props) => {
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const numberClassName = (number) => {
-  if(props.usedNumbers.indexOf(number) >= 0){
-    return 'used';
-  }
-  if(props.selectedNumbers.indexOf(number) >= 0){
-    return 'selected';
-  }
-}
+  const numberClassName = (number) => {
+    if (props.usedNumbers.indexOf(number) >= 0) {
+      return 'used';
+    }
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }
+  };
   return (
-    <div className="numbers">
+    <div className="card text-center">
       <div>
-        {nums.map((number, i) => 
-        <span key={i} className={numberClassName(number)}
-              onClick={() => props.selectNumber(number)}>
-              {number}
-        </span> 
+        {Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
         )}
       </div>
     </div>
   );
-}
+};
+
+Numbers.list = [1,2,3,4,5,6,7,8,9];
 
 export default Numbers;
